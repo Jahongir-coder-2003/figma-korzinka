035c9cb36e29935d2436b0b5b88cc2959ea712c0
Jahongir-coder-2003 <ravshanovj45@gmail.com>
2025-04-22 14:05:41 +0500

    !

--- js/main.js
+++ js/main.js
@@ -1,44 +1,98 @@
-let card = document.createElement("div");
-card.className = "page2-card";
+let pageCards = document.querySelector(".page2-cards");
+let pageCards3 = document.querySelector(".page3-cards");
+let pageCards4 = document.querySelector(".page4-cards");
 
-let cardImg = document.createElement("div");
-cardImg.className = "page2-card-img";
-card.append(cardImg);
-let cardImgimg = document.createElement("img");
-cardImgimg.src = "./images/page2-cardd-img1.png";
-cardImgimg.alt = "CARD IMG NO?";
+function getCards(el) {
+  let card = document.createElement("div");
+  card.className = "page2-card";
 
-cardImg.append(cardImgimg);
-let cardTitle = document.createElement("div");
-cardTitle.className = "page2-card-title";
-card.append(cardTitle);
-let cardTitleh3 = document.createElement("h3");
+  let cardImg = document.createElement("div");
+  cardImg.className = "page2-card-img";
+  card.append(cardImg);
+  let cardImgimg = document.createElement("img");
+  cardImgimg.src = el.images[0];
+  cardImgimg.alt = "CARD IMG NO?";
 
-cardTitleh3.innerText = "44,50 ₽";
+  cardImg.append(cardImgimg);
+  let cardTitle = document.createElement("div");
+  cardTitle.className = "page2-card-title";
+  card.append(cardTitle);
+  let cardTitleh3 = document.createElement("h3");
 
-cardTitle.append(cardTitleh3);
-let cardTitleh4 = document.createElement("h4");
+  cardTitleh3.innerText = el.price += " ₽";
 
-cardTitleh4.innerText = "50,50 ₽";
+  cardTitle.append(cardTitleh3);
+  let cardTitleh4 = document.createElement("h4");
 
-cardTitle.append(cardTitleh4);
+  cardTitleh4.innerText = "50,50 ₽";
 
-let cardInfo = document.createElement("div");
-cardInfo.className = "page2-card-info";
-card.append(cardInfo);
+  cardTitle.append(cardTitleh4);
 
-let cardInfop = document.createElement("p");
-cardInfop.innerText = "С картой";
-cardInfo.append(cardInfop);
-let cardInfop2 = document.createElement("p");
-cardInfop2.innerText = "Обычная";
-cardInfo.append(cardInfop2);
+  let cardInfo = document.createElement("div");
+  cardInfo.className = "page2-card-info";
+  card.append(cardInfo);
 
-let cardh5 = document.createElement("h5");
-cardh5.innerText = "Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...";
-card.append(cardh5);
+  let cardInfop = document.createElement("p");
+  cardInfop.innerText = "С картой";
+  cardInfo.append(cardInfop);
+  let cardInfop2 = document.createElement("p");
+  cardInfop2.innerText = "Обычная";
+  cardInfo.append(cardInfop2);
 
-let cardBtn = document.createElement("a");
-cardBtn.innerHTML = "<button>В КОРЗИНУ</button>";
-card.append(cardBtn);
-console.log(card);
+  let cardh5 = document.createElement("h5");
+  cardh5.innerText = el.description;
+  card.append(cardh5);
+
+  let cardBtn = document.createElement("a");
+  cardBtn.href = "./pages/tovar.html";
+  cardBtn.innerHTML = "<button>В КОРЗИНУ</button>";
+  card.append(cardBtn);
+  pageCards.append(card);
+
+  return card;
+}
+
+let discountProduct = products.filter((el) => el.discount > 0).slice(-4);
+discountProduct.map((el) => pageCards.append(getCards(el)));
+
+let endProduct = products.filter((el) => el).slice(-4);
+endProduct.map((el) => pageCards3.append(getCards(el)));
+
+let ratingProduct = products
+  .filter((el) => el)
+  .slice(-4)
+  .toSorted((a, b) => b.rating - a.rating);
+
+ratingProduct.map((el) => pageCards4.append(getCards(el)));
+
+//page5
+let cards5 = document.querySelector(".page5-cards");
+
+console.log(cards5);
+function getCards5() {
+  let card5 = document.createElement("div");
+  card5.className = "page5-card";
+  cards5.append(card5);
+
+  let cardInfo5 = document.createElement("div");
+  cardInfo5.className = "page5-card-info";
+  card5.append(cardInfo5);
+
+  let card5h2 = document.createElement("h2");
+  card5h2.innerText = "Оформите карту «Северяночка»";
+  cardInfo5.append(card5h2);
+
+  let card5p = document.createElement("p");
+  card5p.innerText = "И получайте бонусы при покупке в магазинах и на сайте";
+  cardInfo5.append(card5p);
+
+  let cardImg5 = document.createElement("div");
+  cardImg5.className = "page5-card-img";
+  card5.append(cardImg5);
+  let cardImg5img = document.createElement("img");
+  cardImg5img.src = "./images/page5-card-img2.png";
+  cardImg5img.alt = "CARD IMG NO?";
+  cardImg5.append(cardImg5img);
+}
+getCards5();
+getCards5();
